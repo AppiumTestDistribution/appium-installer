d3ec69d676d7f552b11550502edc7005ed0448b8
Srinivasan Sekar <srinivasan.sekar1990@gmail.com>
2023-03-29 16:39:52 -0500

    fix emulator launch script

--- src/emulators.ts
+++ src/emulators.ts
@@ -1,18 +1,15 @@
 import inquirer from 'inquirer';
-import { exec } from 'child_process';
-import util from 'util';
-import path from 'path';
-
-const execAsync = util.promisify(exec);
+import { getAllSimulators, launchSimulator } from './ios.js';
+import { getAllEmulators, launchEmulator } from './android.js';
 
 export async function listEmulators() {
   const platform = process.platform;
-  let emulatorCmd, emulatorList, emulatorsOrSimulators;
+  let emulatorList, emulatorsOrSimulators, emulatorID;
 
   if (platform === 'darwin') {
     emulatorsOrSimulators = await inquirer.prompt([
       {
-        type: 'checkbox',
+        type: 'list',
         message: 'Do you want to launch emulators/simulators?',
         name: 'emusim',
         choices: ['Android Emulators', 'iOS Simulators'],
@@ -24,76 +21,51 @@ export async function listEmulators() {
         },
       },
     ]);
-    if (emulatorsOrSimulators.emusim[0] === 'Android Emulators') {
-      emulatorCmd = 'emulator -list-avds';
+    if (emulatorsOrSimulators.emusim === 'Android Emulators') {
+      emulatorList = await getAllEmulators();
     } else {
-      emulatorCmd = 'xcrun simctl list devices';
+      emulatorList = await getAllSimulators();
     }
   } else if (platform === 'win32') {
-    emulatorCmd = 'emulator -list-avds';
+    emulatorList = await getAllEmulators();
   } else {
     console.log('Unsupported platform');
     return;
   }
 
   try {
-    const { stdout } = await execAsync(emulatorCmd);
-    if (emulatorsOrSimulators.emusim[0] === 'iOS Simulators') {
-      emulatorList = stdout
-        .split('\n')
-        .filter((line) => line.includes('(Booted)') || line.includes('(Shutdown)'));
-    } else {
-      emulatorList = stdout.split('\n');
-    }
-    if (emulatorList.length === 0) {
+    if (emulatorList && emulatorList.length === 0) {
       console.log('No emulators found');
       return;
     }
 
     const selectedEmulator = await inquirer.prompt([
       {
-        type: 'checkbox',
+        type: 'list',
         message: 'Select emulators to launch',
         name: 'emulators',
         choices: emulatorList,
         validate(answer) {
           if (answer.length < 1) {
-            return 'You must choose at least one emulator.';
+            return 'You must choose at least one emulator/simulator.';
           }
           return true;
         },
       },
     ]);
-    let emulatorID;
-    const emulatorString = selectedEmulator.emulators[0];
-    if (emulatorsOrSimulators.emusim[0] === 'iOS Simulators') {
-      emulatorID = emulatorString.split('(')[1].split(')')[0];
-    } else {
-      emulatorID = emulatorString;
-    }
+    const emulatorString = selectedEmulator.emulators;
+    console.log(`Launching ${emulatorString}...`);
 
-    console.log(`Launching ${emulatorID}...`);
-    if (platform === 'darwin' && emulatorsOrSimulators.emusim[0] === 'Android Emulators') {
-      const androidHome = process.env.ANDROID_HOME || process.env.ANDROID_SDK_ROOT;
-      if (!androidHome) {
-        console.log('ANDROID_HOME/ANDROID_SDK_ROOT environment variable is not set');
-        return;
-      }
-
-      const emulatorCmd = path.join(androidHome, 'emulator', 'emulator');
-      const avdArgs = `-avd ${emulatorID} `;
-
-      try {
-        await execAsync(`${emulatorCmd} ${avdArgs} -no-window`);
-        console.log(`Emulator ${emulatorID} started successfully.`);
-      } catch (error) {
-        console.error(`Failed to start emulator ${emulatorID}. Error: ${error}`);
-      }
-    } else if (platform === 'darwin' && emulatorsOrSimulators.emusim[0] === 'iOS Simulators') {
-      await execAsync(`xcrun simctl boot ${emulatorID}`);
+    if (platform === 'darwin' && emulatorsOrSimulators.emusim === 'Android Emulators') {
+      await launchEmulator(emulatorString);
+    } else if (platform === 'darwin' && emulatorsOrSimulators.emusim === 'iOS Simulators') {
+      await launchSimulator(
+        emulatorList.find((simulator: { name: any }) => simulator.name === emulatorString).udid
+      );
     } else if (platform === 'win32') {
-      await execAsync(`emulator -avd ${emulatorID}`);
+      await launchEmulator(emulatorString);
     }
+    console.log(`Launching ${emulatorString} completed successfully`);
   } catch (error) {
     console.error(error);
   }
